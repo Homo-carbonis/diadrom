fd5f741f037eccf7ccd2ee90954a37c0e09af750
Homo-carbonis <homocarbonis@protonmail.com>
2023-08-13 10:47:08 +0100

    Work on examples.

--- example.lisp
+++ example.lisp
@@ -2,11 +2,26 @@
 
 ;;; Example 1
 ;;; A simple pendulum modeled as a particle at a fixed length from the origin.
-
 (defsys pendulum (length theta) ((bob (particle)))
   (= theta (angle y-axis bob))
-  (= (distance bob origin) length)
+  (= (bob :r) length)
   (gravity bob))
+
+(defsys particle (v x y z r rho theta phi) ()
+  (= x (elt v 1))
+  (= y (elt v 2))
+  (= z (elt v 3))
+  (= x (* r (sin theta) (cos phi)))
+  (= y (* r (sin theta) (sin phi)))
+  (= z (* r  (cos theta)))
+  (= r (sqrt (+ (sqr x) (sqr y) (sqr z))))
+  (= rho (sqrt (+ (sqr x) (sqr y)))))
+
+(defrule gravity (body)
+  (force 9.81  body))
+
+(defrule force (magnitude body)
+  (= (d (body :v)))
 
 ;; The same but for concision using the name of a system class (ie. 'particle') as the name the subsystem.
 (defsys pendulum (length theta) ((particle))
@@ -38,10 +53,45 @@
   )
 
 ;;; Example 2
-;;; 
+;;; Population
+
+(defsys growth (n rate) ()
+  (= (d n) ((* rate n))))
+
+;;; Example 3
+;;; Bodies
+(defsys body
+
+;;; Example 4
+;;; Playing cards
+
+(defsys card (suit value) ()
+  (in suit (set diamonds hearts spades clubs nil))
+  (or (in value (ordered-set ace 1 2 3 4 5 6 7 8 9 10 jack queen king))
+      (= value joker))
+  (when (= value joker) (= suit nil)))
+
+(defsys pack (n) (((card) :repeat n))
+  (when (not (= (value card) joker)) (unique card))
+  (count (= (value card) joker) 2)
+  (max n))
+
+;OR
+
+(defset pack card
+  (when (not (= (value card) joker)) (unique card))
+  (count (= (value card) joker) 2)
+  (max n)) ; Anaphor n for size of set
+
+; Maybe focus on continuous dynamical systems
+
 
 ;;; Design Points
 
-;; Dimensionality should be transparent. A `particle` object should represent an n-dimensional particle and n should be determined automatically. Some special systems or objects undefinable in dyn itself will be needed to allow this.
+;; defsys macros are a wrapper around rules language
+; (defsys (x) () ...), when applied, will expand to: ((= x ...) ...)
 
-; There needs to be a a way to handle mutually dependent variables. For instance a particle can have polar and cartesian coordinate values.
+
+;; Maybe try to simplify lisp syntax and standards where possible since we are not writing general purpose programs and potential users may not be lispers. Try not to disrupt standard CL in doing so though.
+; Interpret an unquoted list as a quoted list where the first element does not represent a function.
+; Allow = to be used for any equality test and try to do the right thing.
