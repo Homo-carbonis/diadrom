64abca28c26748aa9422a567e86239c25344094c
Homo-carbonis <homocarbonis@protonmail.com>
2023-10-22 11:51:35 +0100

    Fix domain-union

--- domain.lisp
+++ domain.lisp
@@ -7,25 +7,32 @@
 
 (in-package :dyn/domain)
 
-(defun domain-union (d1 d2 &optional acc)
+(defun domain-union (d1 d2)
+  (reverse (%domain-union d1 d2 nil)))
+
+(defun %domain-union (d1 d2 acc)
   (cond
     ((and d1 d2) 
      (let ((a (car d1))
            (b (car d2)))
        (ecase (interval-relation a b)
          (a<b
-           (domain-union (cdr d1) d2 (cons a acc)))
+           (%domain-union (cdr d1) d2 (cons a acc)))
          (a-intersects-b
            (let ((u (cons (car a) (cdr b))))
-             (domain-union (cdr d1) (cons u (cdr d2)) acc)))
+             (%domain-union (cdr d1) (cons u (cdr d2)) acc)))
          (b-subset-a 
-           (domain-union d1 (cdr d2) (cons a acc))) 
+           (%domain-union d1 (cdr d2) acc)) 
          (a-subset-b
-           (domain-union (cdr d1) d2 (cons b acc)))
+           (%domain-union (cdr d1) d2 acc))
          (b-intersects-a
            (let ((u (cons (car b) (cdr a))))
-             (domain-union (cons u (cdr d1)) d2 acc)))
-         (a>b (domain-union d1 (cdr d2) (cons b acc))))))))
+             (%domain-union (cons u (cdr d1)) d2 acc)))
+         (a>b (%domain-union d1 (cdr d2) (cons b acc))))))
+    (d1 (append d1 acc))
+    (d2 (append d2 acc))
+    (t acc)))
+
 
 (defun interval-relation (a b)
   (if (< (car a) (car b))
